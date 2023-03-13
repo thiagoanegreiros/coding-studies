4456d0b784cfc447c0041d716662e59da963fb62
Thiago Ananias <thiagoananias@gmail.com>
2023-03-13 17:05:39 -0300

    First solution

--- two-sum/index.js
+++ two-sum/index.js
@@ -1,3 +1,24 @@
-const a = 1
+const assert = require('assert')
 
-console.info(a)
+const twoSum = (items, target) => {
+    const results = {}
+    for (let i=0;i<items.length;i++) {
+        const itemToBeExpected = target - items[i]
+        if (results[itemToBeExpected] !== undefined) {
+            return [
+                results[itemToBeExpected], i
+            ]
+        } else {
+            results[items[i]] = i
+        }
+    }
+    return undefined
+}
+
+const entry01 = [[2, 7, 11, 15], 9]
+assert.deepEqual(twoSum(entry01[0], entry01[1]), [0, 1])
+
+const entry02 = [[2, 4, 5, 7, 11, 15], 18]
+assert.deepEqual(twoSum(entry02[0], entry02[1]), [3, 4])
+
+console.info('Success')
