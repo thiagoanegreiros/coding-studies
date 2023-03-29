eb64586777efc41518c2e07a6d45b7bcd253d4e5
Thiago Ananias <thiagoananias@gmail.com>
2023-03-29 12:33:59 -0300

    Ongoing

---
+++ arrays/interview-02.js
@@ -0,0 +1,112 @@
+const assert = require('assert')
+
+/* Q1
+You are in charge of the cake for your niece's birthday and have decided the cake will have one candle for each year of her total age. 
+When she blows out the candles, she’ll only be able to blow out the tallest ones. Your task is to find out how many candles she can successfully blow off.
+	
+For example, if your niece is turning 4 years old, and the cake will have 4 candles of height 4, 4 , 1, 3, she will be able to blow out 2 candles
+successfully, since the tallest candles are of height of height 4 and there are 2 such candles.
+
+Example 2: 
+4
+3,2,1,3 
+The result is 2 because the tallest candles are of height 3 and they are only 2.
+
+Example 3:
+7
+3,2,1,3,6,2,1
+The result is 1 because the tallest candle is of height 6 and there's only 1.
+
+*/
+
+const blowCandles = (candles) => {
+  let greater = 0
+  let count = 0
+  for(let i=0;i<candles.length;i++) {
+    if (candles[i] > greater) {
+      count = 1
+      greater = candles[i]
+    } else if (candles[i] === greater) {
+      count++
+    }
+  }
+
+  return count
+}
+
+assert.deepEqual(blowCandles([4, 4 ,1 ,3]), 2)
+assert.deepEqual(blowCandles([3, 2, 1, 3]), 2)
+assert.deepEqual(blowCandles([3, 2, 1, 3, 6, 2, 1]), 1)
+
+console.info('success 01')
+  
+/* Q2
+
+Prime number is a number that is divisible by only 1 and itself
+1 is not a prime number
+2 is a prime number
+Write a method that determines if a number is a prime number or not
+Return true if it is a prime number, else return false
+*/
+const isPrimeNumber = (number) => {
+  for (let i=2;i<number;i++)
+    if (number % i === 0) 
+      return false
+  return true
+}
+
+assert.deepEqual(isPrimeNumber(1), true)
+assert.deepEqual(isPrimeNumber(2), true)
+assert.deepEqual(isPrimeNumber(5), true)
+assert.deepEqual(isPrimeNumber(13), true)
+assert.deepEqual(isPrimeNumber(17), true)
+assert.deepEqual(isPrimeNumber(4), false)
+assert.deepEqual(isPrimeNumber(6), false)
+assert.deepEqual(isPrimeNumber(97), true)
+assert.deepEqual(isPrimeNumber(89), true)
+assert.deepEqual(isPrimeNumber(10), false)
+
+console.info('success 02')
+
+/* Q3
+Assume we have a list of words from the English dictionary, like: 
+
+EnglishWords:["water","big","apple","watch","banana","york","amsterdam","orange","macintosh","bottle","book"]
+
+And another long list of string to process, write a function to identify ""compound words"" and return them:
+
+input: ["paris","applewatch","ipod","amsterdam","bigbook","orange","waterbottle"]
+output: ["applewatch","bigbook","waterbottle"] 
+
+*/
+
+const findCompoundWords = (input) => {
+  const englishWords = ["water", "big", "apple", "watch", "banana", "york", "amsterdam", "orange", "macintosh", "bottle", "book"]
+  const output = []
+
+  for (let i=0;i<input.length;i++) {
+    const item = input[i]
+    let count = 0
+    for (let j=0;j<englishWords.length;j++) {
+      const eItem = englishWords[j]
+      if (item.indexOf(eItem) > -1) {
+        count++
+      }
+      if (count > 1) {
+        output.push(item)
+        break
+      }
+    }
+  }
+
+  return output
+}
+
+assert.deepEqual(
+  findCompoundWords(
+    ["paris","applewatch","ipod","amsterdam","bigbook","orange","waterbottle"]
+  ),
+  ["applewatch","bigbook","waterbottle"]
+)
+
+console.info('success 03')
--- trees/countGoodNodesInBinaryTree.js
+++ trees/countGoodNodesInBinaryTree.js
@@ -1,19 +1,31 @@
 const assert = require('assert')
-const { buildTreeLeekCode } = require('./helper')
 
-const goodNodes = function(root, max = -Infinity, total = [ 0 ]) {
-    count(root, max, total);
-    return total[0]
+const prefixes = new Map()
+
+const countAllPathsWithModK0 = function(k, costs, from_lst, to_lst) {
+    const children = new Map();
+    prefixes.set(from_lst[0], costs[0])
+    for (let i = 0; i < from_lst.length; i++) {
+      const u = from_lst[i]
+      if (!children.has(u)) {
+        children.set(u, [])
+      }
+      children.get(u).push({
+        cost: costs[i+1],
+        node: to_lst[i]
+      })
+    }
+    return count(children, k)
 }
 
-const count = (root, max, total) => {
-    const isBaseCase = root === null;
+const count = (root, k) => {
+    const isBaseCase = root === null
     if (isBaseCase) return 0;
 
-    return dfs(root, max, total);
+    return dfs(root, k)
 }
 
-const dfs = (root, max, total) => {
+const dfs = (root, k) => {
     const isGood = max <= root.val
     if (isGood) total[0]++;
 
@@ -23,7 +35,11 @@ const dfs = (root, max, total) => {
     count(root.right, max, total);
 }
 
-assert.deepEqual(goodNodes(buildTreeLeekCode([3,1,4,3,null,1,5])), 4)
-assert.deepEqual(goodNodes(buildTreeLeekCode([3,3,null,4,2])), 3)
+assert.deepEqual(countAllPathsWithModK0(3, [2,1,2,2,4,2,5], [0,0,1,1,2,2], [1,2,3,4,5,6]), 4)
+// assert.deepEqual(countAllPathsWithModK0(2, [1,2,2,1,2], [2,2,1,2], [3,1,4,5]), 6)
+// assert.deepEqual(countAllPathsWithModK0(3, [2,3,0,3,0], [2,3,3,3], [3,1,4,5]), 7)
+
+// assert.deepEqual(countAllPathsWithModK0(2, [1,1,1,1], [0,0,3], [1,3,2]), 3)
+// assert.deepEqual(countAllPathsWithModK0(2, [1,2,2,1,2], [0,0,1,1], [1,3,2,4]), 6)
 
 console.info('Success')
