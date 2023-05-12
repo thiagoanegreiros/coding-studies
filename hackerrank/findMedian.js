f6c293e992c6b5ce85a4cdf28a76750105700464
Thiago Ananias <thiagoananias@gmail.com>
2023-05-12 11:28:03 -0300

    sort not working properly

--- hackerrank/findMedian.js
+++ hackerrank/findMedian.js
@@ -1,7 +1,7 @@
 const assert = require('assert')
 
 function findMedian(arr) {
-    const sa = arr.sort()
+    const sa = arr.sort((a, b) => (a - b))
     if (sa.length % 2 === 0) {
         const ix = sa.length / 2
         return (sa[ix - 1] + sa[ix]) / 2
