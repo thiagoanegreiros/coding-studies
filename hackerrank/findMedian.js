03e10afc2a95c725b49b5593b31e78beb5201f9d
Thiago Ananias <thiagoananias@gmail.com>
2023-05-12 11:16:30 -0300

    FindMedian

---
+++ hackerrank/findMedian.js
@@ -0,0 +1,14 @@
+const assert = require('assert')
+
+function findMedian(arr) {
+    const sa = arr.sort()
+    if (sa.length % 2 === 0) {
+        const ix = sa.length / 2
+        return (sa[ix - 1] + sa[ix]) / 2
+    } else {
+        return sa[Math.floor(sa.length / 2)]
+    }
+}
+
+assert.equal(findMedian([1, 2, 3, 4, 5]), 3)
+assert.equal(findMedian([0, 1, 4, 6, 7, 8]), 5)
