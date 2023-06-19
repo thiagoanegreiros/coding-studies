c1936f077f538207034490c77cbb331d2c5bf8f5
Thiago Ananias <thiagoananias@gmail.com>
2023-06-19 07:57:00 -0300

    Compose in one line

--- ts-src/compose.ts
+++ ts-src/compose.ts
@@ -5,12 +5,7 @@ const add2: numberFunction = (val: number) => val + 2
 const mult2: numberFunction = (val: number) => val * 2
 const div2: numberFunction = (val: number) => val / 2
 
-const compose = (...funcs: numberFunction[]) => (number: number): number => {
-    return funcs.reduceRight((acc: number, func: numberFunction) => {
-        console.info(`NOW ==> ${acc}`)
-        return func(acc)
-    }, number)
-}
+const compose = (...funcs: numberFunction[]) => (number: number) => funcs.reduceRight((acc, func) => func(acc), number)
 
 const math = compose(add1, div2, add2, mult2)(2)
 
